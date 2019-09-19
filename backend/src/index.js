5db7b127a4080fdf3a11c264922d32ba4b05be8c
rampemus <pasi.toivanen@live.com>
2019-09-19 15:04:39 +0300

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,44 @@
+const debug = require('debug')('weathermap');
+require('dotenv').config();
+
+const Koa = require('koa');
+const router = require('koa-router')();
+const fetch = require('node-fetch');
+const cors = require('kcors');
+
+const appId = process.env.APPID;
+console.log('process environment value APPID:',appId)
+const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
+const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
+
+const port = process.env.PORT || 9000;
+
+const app = new Koa();
+
+app.use(cors());
+
+const fetchWeather = async () => {
+    console.log('got request, now going to endpoint')
+    console.log('mapURI:',mapURI)
+    console.log('targetCity:',targetCity)
+    console.log('appid:',appId)
+
+  const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}`;
+  const response = await fetch(endpoint);
+  console.log('got the data! now response', response)
+  return response ? response.json() : {}
+};
+
+router.get('/api/weather', async ctx => {
+  const weatherData = await fetchWeather();
+
+  ctx.type = 'application/json; charset=utf-8';
+  ctx.body = weatherData.weather ? weatherData.weather[0] : {};
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(port);
+
+console.log(`App listening on port ${port}`);
---
+++ frontend/src/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const baseURL = 'http://localhost:9000/api';
+
+const getWeatherFromApi = async () => {
+	try {
+		const response = await fetch(`${baseURL}/weather`);
+        return response.json();
+	} catch (error) {
+		console.error(error);
+	}
+
+	return {};
+};
+
+class Weather extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			icon: ""
+		};
+	}
+
+	async componentWillMount() {
+		const weather = await getWeatherFromApi();
+		this.setState({
+			icon: weather.icon.slice(0, -1)
+		});
+	}
+
+	render() {
+		const {icon} = this.state;
+
+		return (<div className="icon">
+			{icon && <img src={`/img/${icon}.svg`}/>}
+		</div>);
+	}
+}
+
+ReactDOM.render(<Weather/>, document.getElementById('app'));
