973181f7ef00cec917ae34712412dad8e4565820
rampemus <pasi.toivanen@live.com>
2019-09-26 11:21:03 +0300

    Refactored previoutRequests to middleware

--- backend/src/controllers/forecastRouter.js
+++ backend/src/controllers/forecastRouter.js
@@ -1,14 +1,6 @@
 const Router = require('koa-router');
 const fetch = require('node-fetch');
 
-const fetchRefreshInterval = process.env.REFRESH_INTERVAL || 10; // minutes
-let previousRequests = [];
-const removeOldData = () => {
-  const oldestValidTime = new Date(new Date() - 1000 * 60 * fetchRefreshInterval);
-  const firstOld = previousRequests.map(fetch => fetch.timestamp).findIndex(time => new Date(time) < oldestValidTime);
-  firstOld === -1 ? console.log('data is up to date') : previousRequests = previousRequests.slice(0, firstOld);
-};
-
 const appId = process.env.APPID;
 const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
 const fetchForecast = async (id) => {
@@ -21,26 +13,10 @@ const forecastRouter = new Router();
 
 forecastRouter.post('/api/forecast', async ctx => {
   const id = parseInt(ctx.request.query.id);
+  const weatherData = await fetchForecast(id); // TODO: use id
 
-  removeOldData();
-
-  const previousForecast = previousRequests.find((request) => request.id === id && request.type === 'forecast');
-
-  if (previousForecast) {
-    ctx.type = 'application/json; charset=utf-8';
-    ctx.body = previousForecast.data.list ? previousForecast.data : {};
-  } else {
-    const weatherData = await fetchForecast(id); // TODO: use id
-    previousRequests.unshift({
-      id: weatherData.id,
-      type: 'forecast',
-      data: weatherData,
-      timestamp: new Date(),
-    });
-
-    ctx.type = 'application/json; charset=utf-8';
-    ctx.body = weatherData.list ? weatherData : {};
-  }
+  ctx.type = 'application/json; charset=utf-8';
+  ctx.body = weatherData.list ? weatherData : {};
 });
 
 module.exports = forecastRouter;
--- backend/src/controllers/weatherRouter.js
+++ backend/src/controllers/weatherRouter.js
@@ -1,14 +1,6 @@
 const Router = require('koa-router');
 const fetch = require('node-fetch');
 
-const fetchRefreshInterval = process.env.REFRESH_INTERVAL || 10; // minutes
-let previousRequests = [];
-const removeOldData = () => {
-  const oldestValidTime = new Date(new Date() - 1000 * 60 * fetchRefreshInterval);
-  const firstOld = previousRequests.map(fetch => fetch.timestamp).findIndex(time => new Date(time) < oldestValidTime);
-  firstOld === -1 ? console.log('data is up to date') : previousRequests = previousRequests.slice(0, firstOld);
-};
-
 const appId = process.env.APPID;
 const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
 const fetchWeather = async (id) => {
@@ -21,29 +13,9 @@ const weatherRouter = new Router();
 
 weatherRouter.post('/api/weather', async ctx => {
   const id = parseInt(ctx.request.query.id);
-
-  removeOldData();
-
-  const oldWeather = previousRequests.find((request) => request.id === id && request.type === 'weather');
-
-  if (oldWeather) {
-    console.log('same query was done within 10 minutes, sending duplicate');
-    ctx.type = 'application/json; charset=utf-8';
-    ctx.body = oldWeather.data.weather ? oldWeather.data : {};
-  } else {
-    console.log('Query id is new');
-
-    const weatherData = await fetchWeather(id); // TODO: use id
-    previousRequests.unshift({
-      id: weatherData.id,
-      type: 'weather',
-      data: weatherData,
-      timestamp: new Date(),
-    });
-
-    ctx.type = 'application/json; charset=utf-8';
-    ctx.body = weatherData.weather ? weatherData : {};
-  }
+  const weatherData = await fetchWeather(id);
+  ctx.type = 'application/json; charset=utf-8';
+  ctx.body = weatherData.weather ? weatherData : {};
 });
 
 module.exports = weatherRouter;
